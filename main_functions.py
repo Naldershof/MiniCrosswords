06e2e2bd60d3e76c783f23c8081129e1bdc4068a
Nick Aldershof <Naldershof@gmail.com>
2020-03-17 23:13:40 -0400

    Update main_functions.py

--- main_functions.py
+++ main_functions.py
@@ -2,40 +2,35 @@ import numpy as np
 import nltk
 import re
 from nltk.corpus import wordnet as wn
+import tqdm
+import itertools
+
+# Right now this just works with 5x5, there's no reason you can't change it though, 
+# it's just not super optimized just yet
+
+# Global
+MATCH_DICT = {}
 
 starting_words = ['TRUMP', 'PENCE']
 gridsize = [5,5]
 grid = np.full(gridsize, fill_value='.', dtype=str)
 
-word_list = list(wn.words())
+word_list = list(brown.words()) + list(words.words())
 word_list = [x.upper() for x in word_list]
+
+word_limits = gridsize
+word_list = list(filter(lambda x: (len(x) >= word_limits[0]) and (len(x) <= word_limits[0]), word_list))
 
 word_list.extend(starting_words)
 word_list = list(set(word_list))
 
-# Change this so that it can be in other positions other than 0 and 4
 grid[0] = list(starting_words[0])
-# TODO: I forget if this is row or column size, check
 grid[gridsize[0] - 1] = list(starting_words[1])
 
+MATCH_DICT = {}
 
-# So we're going to check that there's some possible solution at least vertically for everything
-# This should be extended to an arbitrary function to see if we can use it to iterate through
-
-option_length = {}
-
-# Getting length of possible options vertically. 
-for x in range(0, gridsize[0]):
-    match_str = '^'+''.join(grid[:,x])+'$'
-    regex = re.compile(match_str)
-    options = len(list(filter(regex.match, word_list)))
-    option_length[x] = options
-
-#Start iterating for the option with the least 
-key_min = min(option_length.keys(), key=(lambda k: option_length[k]))
-
-# Get possibile answers for a row or column
 def word_options(grid, column=None, row=None):
+    # For a given row or column get the possible options
     if row and column:
         return Exception('Can only set one of row or column')
     elif row is not None:
@@ -44,12 +39,21 @@ def word_options(grid, column=None, row=None):
         match_str = '^'+''.join(grid[:,column])+'$'
     else:
         return Exception('Must set one of row or column')
-    regex = re.compile(match_str)
-    options = list(filter(regex.match, word_list))
-    #TODO: Remove words that have special characters
+    
+    global MATCH_DICT
+    
+    if match_str in MATCH_DICT.keys():
+        options = MATCH_DICT[match_str]
+    else:
+        regex = re.compile(match_str)
+        options = list(filter(regex.match, word_list))
+        #TODO: Remove words that have special characters
+        match_dict[match_str] = options
     return options
 
 def check_grid(grid, verbose=False, boolean=False):
+    # Check that each unfilled row or column has at least one valid way it can be filled in
+    # Or if it's a full grid check to make sure that it's only full of valid words
     row_lengths = {}
     for row in range(0, grid.shape[0]):
         rl = len(word_options(grid, row=row))
@@ -61,6 +65,7 @@ def check_grid(grid, verbose=False, boolean=False):
             if verbose:
                 print('Failed on Row {}'.format(row))
             return False
+    
     col_lengths = {}
     for col in range(0, grid.shape[0]):
         cl = len(word_options(grid, column=col))
@@ -72,75 +77,64 @@ def check_grid(grid, verbose=False, boolean=False):
             if verbose:
                 print('Failed on Column {}'.format(col))
             return False
+    
     if boolean:
         return True
     else:
         return row_lengths, col_lengths
-
-import tqdm
-
-def add_valid_cols(grid):
-    row_lengths, col_lengths = check_grid(grid)
-
+    
+def add_valid_entries(grid, row=False, column=False, amount=1):
     # So let's try the smallest two options, figure out which are valid grids
-    key_min = min(col_lengths.keys(), key=(lambda k: col_lengths[k]))
-    del col_lengths[key_min]
-    key_second = min(col_lengths.keys(), key=(lambda k: col_lengths[k]))
-
-    short_col_options = word_options(grid, column=key_min)
-    second_shortest_col_options = word_options(grid, column=key_second)
-
-    valid_grids = []
-    for word in short_col_options:
-        for word2 in second_shortest_col_options:
-            new_grid = grid.copy()
-            new_grid[:, key_min] = list(word)
-            new_grid[:, key_second] = list(word2)
-            #print(new_grid)
-            if check_grid(new_grid, boolean=True):
-                valid_grids.append(new_grid)
-                
-    return valid_grids
-
-
-#####
-#
-# TODO : This really needs to account for the fact that you can't always add 2 at once
-#
-##### 
-
-def add_valid_rows(grid):
+    if row and column:
+        return Exception('Only one of row or column may be set')
+    elif (not row) and (not column):
+        return Exception('One of row or column must be set')
+    
     row_lengths, col_lengths = check_grid(grid)
-
-    # So let's try the smallest two options, figure out which are valid grids
-
-    try:
-        key_min = min(row_lengths.keys(), key=(lambda k: row_lengths[k]))
-        del row_lengths[key_min]
-        key_second = min(row_lengths.keys(), key=(lambda k: row_lengths[k]))
-    except:
+    
+    if row:
+        lengths = row_lengths
+    if column:
+        lengths = col_lengths
+        
+    keys = sorted(lengths, key=lengths.get)[0:amount]
+    
+    if len(lengths) < amount:
         return None
-
-    short_row_options = word_options(grid, row=key_min)
-    second_shortest_row_options = word_options(grid, row=key_second)
-
+    
+    suggestion_lists = []
+    for num in keys:
+        if row:
+            suggestion_lists.append(word_options(grid, row=num))
+        if column:
+            suggestion_lists.append(word_options(grid, column=num))
+    
+    if amount == 1:
+        product_list = list(itertools.product(suggestion_lists[0], repeat=1))
+    elif amount > 1:
+        product_list = list(itertools.product(*suggestion_lists))
+    else: 
+        return Exception('Incorrect amount parameter')
+    
+    #print(product_list)
     valid_grids = []
-    for word in short_row_options:
-        for word2 in second_shortest_row_options:
-            new_grid = grid.copy()
-            new_grid[key_min, :] = list(word)
-            new_grid[key_second, :] = list(word2)
-            #print(new_grid)
-            if check_grid(new_grid, boolean=True):
-                valid_grids.append(new_grid)
+    for entry in product_list:
+        new_grid = grid.copy()
+        for index, col in enumerate(keys):
+            if row:
+                new_grid[col, :] = list(entry[index])
+            if column:
+                new_grid[:, col] = list(entry[index])
+        if check_grid(new_grid, boolean=True):
+            valid_grids.append(new_grid)
                 
     return valid_grids
 
-valid_grids_col = add_valid_cols(grid)
-valid_grids_rows = []
-for v_grid in tqdm.tqdm(valid_grids_col):
-    res = add_valid_rows(v_grid)
+valid_grids_col = add_valid_entries(grid, column=True, amount=3)
+final_grids = []
+for tgrid in tqdm.tqdm(valid_grids_col):
+    res = add_valid_entries(tgrid, row=True, amount=3)
     if res:
-        valid_grids_rows.extend(res)
+        final_grids.extend(res)
     else:
         pass
