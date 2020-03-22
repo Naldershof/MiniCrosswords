176dbb9b0133909ea95dd156b24a7729053e7114
Nick Aldershof <naldershof@okcupid.com>
2020-03-21 23:49:29 -0400

    just some general updates

--- main_functions.py
+++ main_functions.py
@@ -1,29 +1,37 @@
 import numpy as np
 import nltk
+import tqdm
 import re
 
 nltk.download('brown')
 nltk.download('wordnet')
+nltk.download('reuters')
+nltk.download('names')
+nltk.download('webtext')
 
 from nltk.corpus import wordnet as wn
 from nltk.corpus import brown
-from nltk.corpus import words
+from nltk.corpus import reuters
+from nltk.corpus import names
+from nltk.corpus import webtext
 
+from nltk.corpus import words
 import itertools
-import tqdm
 
 # Right now this just works with 5x5, there's no reason you can't change it though, 
-# it's just not super optimized just yet
+# Just need to spend the time to actually make that work
 
 # Global
 MATCH_DICT = {}
 
-starting_words = ['TRUMP', 'PENCE']
-gridsize = [5,5]
+sgridsize = [5,5]
 grid = np.full(gridsize, fill_value='.', dtype=str)
 
-word_list = list(brown.words()) + list(words.words())
-word_list = [x.upper() for x in word_list]
+#word_list = list(brown.words())
+#word_list = list(wn.words()) + list(words.words())
+#word_list = list( set( list(reuters.words()) + list(brown.words() + list(names.words()) + list(webtext.words())) ))
+word_list = list( set( list(reuters.words()) + list(brown.words()) ))
+word_list = [x.upper() for x in word_list if x.isalnum()]
 
 word_limits = gridsize
 word_list = list(filter(lambda x: (len(x) >= word_limits[0]) and (len(x) <= word_limits[0]), word_list))
@@ -31,13 +39,10 @@ word_list = list(filter(lambda x: (len(x) >= word_limits[0]) and (len(x) <= word
 word_list.extend(starting_words)
 word_list = list(set(word_list))
 
-grid[0] = list(starting_words[0])
-grid[gridsize[0] - 1] = list(starting_words[1])
 
 MATCH_DICT = {}
 
 def word_options(grid, column=None, row=None):
-    # For a given row or column get the possible options
     if row and column:
         return Exception('Can only set one of row or column')
     elif row is not None:
@@ -55,12 +60,10 @@ def word_options(grid, column=None, row=None):
         regex = re.compile(match_str)
         options = list(filter(regex.match, word_list))
         #TODO: Remove words that have special characters
-        match_dict[match_str] = options
+        MATCH_DICT[match_str] = options
     return options
 
 def check_grid(grid, verbose=False, boolean=False):
-    # Check that each unfilled row or column has at least one valid way it can be filled in
-    # Or if it's a full grid check to make sure that it's only full of valid words
     row_lengths = {}
     for row in range(0, grid.shape[0]):
         rl = len(word_options(grid, row=row))
@@ -97,7 +100,10 @@ def add_valid_entries(grid, row=False, column=False, amount=1):
     elif (not row) and (not column):
         return Exception('One of row or column must be set')
     
-    row_lengths, col_lengths = check_grid(grid)
+    try:
+        row_lengths, col_lengths = check_grid(grid)
+    except:
+        return None
     
     if row:
         lengths = row_lengths
@@ -137,11 +143,33 @@ def add_valid_entries(grid, row=False, column=False, amount=1):
                 
     return valid_grids
 
-valid_grids_col = add_valid_entries(grid, column=True, amount=3)
-final_grids = []
-for tgrid in tqdm.tqdm(valid_grids_col):
-    res = add_valid_entries(tgrid, row=True, amount=3)
-    if res:
-        final_grids.extend(res)
-    else:
-        pass
+def generate_list(length=50):
+    while True:
+        grid = np.full(gridsize, fill_value='.', dtype=str)
+        starting_words = [random.choice(word_list), random.choice(word_list)]
+        print(time.asctime())
+        print('{} - {}'.format(starting_words[0], starting_words[1]))
+        
+        grid[0] = list(starting_words[0])
+        # TODO: I forget if this is row or column size, check
+        grid[gridsize[0] - 1] = list(starting_words[1])
+
+        valid_grids_col = add_valid_entries(grid, column=True, amount=3)
+        
+        if valid_grids_col is None:
+            continue
+        
+        final_grids = []
+        for tgrid in valid_grids_col:
+            res = add_valid_entries(tgrid, row=True, amount=3)
+            if res:
+                final_grids.extend(res)
+            else:
+                pass
+        if len(final_grids) > 0:
+            print('Success!')
+            all_grids.extend(final_grids)
+            print(len(all_grids))
+        if len(all_grids) >= length:
+            break
+    return all_grids
